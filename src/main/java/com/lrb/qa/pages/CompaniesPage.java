743ae5742fc8bcaa9b418c58d2e1ca5011046f56
ludwk123 <ludwk123@comcast.net>
2018-02-23 15:16:30 -0500

    Added classes and testdata

---
+++ src/main/java/com/lrb/qa/pages/CompaniesPage.java
@@ -0,0 +1,6 @@
+package com.lrb.qa.pages;
+
+import com.lrb.qa.base.TestBase;
+
+public class CompaniesPage extends TestBase {
+}
---
+++ src/main/java/com/lrb/qa/pages/HomePage.java
@@ -0,0 +1,6 @@
+package com.lrb.qa.pages;
+
+import com.lrb.qa.base.TestBase;
+
+public class HomePage extends TestBase {
+}
