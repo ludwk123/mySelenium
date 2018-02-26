e4335e6dc6d0a8c3b8ce3b518aa23772223e009f
ludwk123 <ludwk123@comcast.net>
2018-02-26 08:06:17 -0500

    Added CalendarPageTest

---
+++ src/test/java/com/lrb/qa/testcases/CalendarPageTest.java
@@ -0,0 +1,20 @@
+package com.lrb.qa.testcases;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CalendarPageTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testTest1() throws Exception {
+
+        System.out.println("testTest1");
+
+    }
+}
